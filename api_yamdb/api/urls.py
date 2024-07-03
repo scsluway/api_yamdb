dc6a854a2e4694ce6bca2d80094186859c5460e6
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-07-03 16:52:37 +0300

    Created models: Category, Title, Genre (need to complete the Title model).

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+
+# v1_router = SimpleRouter()
+# v1_router.register('categories', views.CategoryViewSet, basename='category')
+# v1_router.register('genres', views.GenreViewSet, basename='genre')
+# v1_router.register('titles', views.TitleViewSet, basename='title')
+
+
+# v1_urls = [
+#     path('', include(v1_router.urls)),
+# ]
+
+# urlpatterns = [
+#     path('v1/', include(v1_urls)),
+# ]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name[:20]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name[:20]
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.DateTimeField()
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True,
+    )
+    genre = models.IntegerField()  # затычка
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name[:20]
