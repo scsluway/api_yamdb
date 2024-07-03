57b9e01899743ac548a46223e14dba5280659c04
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-07-03 21:35:54 +0300

    Serializers in progress.

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        
